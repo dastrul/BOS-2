4a59f1ece72eb0c6cc8eb626e7141baa4c5c571f
dastrul <vanerk11@yandex.ru>
2024-06-01 20:47:09 +0300

    Update liblogger.cpp

--- src/liblogger.cpp
+++ src/liblogger.cpp
@@ -1,1 +1,9 @@
 #include <iostream>
+void getjsonlog()
+{
+  std::cout<<"{\"loglvl\":\"INFO\",\"message\":\"test message from library\"}"<<std::endl;
+}
+void writejsonlog(std::string& in)
+{
+  std::cout<<"Hello from library  \n"<<input<<std::endl;
+}
